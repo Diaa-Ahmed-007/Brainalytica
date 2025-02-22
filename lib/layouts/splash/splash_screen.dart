8667e6749be48fc432c587ecb56a28fc37932326
Diaa-Ahmed-007 <bekoahmed738@gmail.com>
2025-02-22 03:44:15 +0200

    feature flows screen UI

--- lib/layouts/splash/splash_screen.dart
+++ lib/layouts/splash/splash_screen.dart
@@ -1,12 +1,44 @@
+import 'package:doctors/core/utils/assets.dart';
+import 'package:doctors/core/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+   @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+    _navigatetohome() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        // checkAutoLogin();
+         Navigator.of(context).pushReplacementNamed(Routes.flowscreenRouteName);
+      },
+    );
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.asset(
+            Assets.assetsImagesSplash,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
     );
   }
 }
