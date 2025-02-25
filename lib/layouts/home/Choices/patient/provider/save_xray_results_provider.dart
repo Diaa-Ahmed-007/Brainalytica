b2481cf5607fd2099825e3f5b368a7ef3b3633bd
Diaa-Ahmed-007 <bekoahmed738@gmail.com>
2025-02-25 23:28:43 +0200

    some changes

---
+++ lib/layouts/home/Choices/patient/provider/save_xray_results_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SaveXrayResultsProvider extends ChangeNotifier{
+  String strokeDetection="";
+  String strokeClassification="";
+
+  void changeStrokeDetection(String strokeDetection){
+    this.strokeDetection=strokeDetection;
+    notifyListeners();    
+  }
+
+  void changeStrokeClassification(String strokeClassification){
+    this.strokeClassification=strokeClassification;
+    notifyListeners();    
+  }
+}
